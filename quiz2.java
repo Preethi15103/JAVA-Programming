555ed1ffad5381a8ea64b4add545013b0a8d1497
Preethi <preethiprithu1510@gmail.com>
2026-05-01 11:40:09 +0530

    Initial commit

---
+++ quiz2.java
@@ -0,0 +1,17 @@
+class quiz2{
+    public static void main(String[] args){
+        int x = 5; 
+        int y = 5;
+        System.out.println(--x);
+        System.out.println(y++);
+        int z = 5; 
+        System.out.println(++z);
+        int a = 5; 
+        int b = a++ + ++a; 
+        System.out.println(b);
+        //final int c = 10; 
+        //c = 20;
+        int d = 5; 
+        System.out.println(d--);
+    }
+}
